50c121ef4d2607fceac2a5046a5310a1dc27c0cd
neuroiwm <seitaro.iwama@gmail.com>
2023-03-22 10:22:03 +0900

    fix:path

--- test_heart_rate.py
+++ test_heart_rate.py
@@ -4,13 +4,13 @@ from atom import ATOM
 if __name__ == '__main__':
     atom = ATOM()
     oura = OURA()
-    params ={ 
+    params = {
             'start_datetime': '2023-02-22T10:00:00+09:00',
-            'end_datetime': '2023-02-24T00:00:00+09:00'
+            'end_datetime': '2023-03-22T00:00:00+09:00'
             }
     oura.set_params(params=params)
     df = oura.get_pd()
     name_oura = 'oura_HR_' +  atom.get_now() + '.csv'
-    df.to_csv('./',index=False)
+    df.to_csv('./' + name_oura, index=False)
     print(df)
     print('data acquired')
